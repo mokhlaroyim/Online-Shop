f2a4212ed9bff0cc8f272c07775feec445f45ace
Mokhlaroyim <mokhlaroyim097@gmail.com>
2020-09-04 23:02:29 +0500

    initial commit

---
+++ src/style.js
@@ -0,0 +1,26 @@
+// $(document).ready(function(){
+//     $("#onclick-btn").click(function(){
+//         var = document.createElement("<div>");
+
+//     });
+// });
+$(document).ready(function(){
+	$("#buy-btn").click(function(){
+		$("#myModal").modal();
+	});
+});
+$(document).ready(function() {
+     
+            $(".active").click(function() {
+               $(".search-box").toggle();
+               $("input[type='text']").focus();
+             });
+ 
+        });
+// $(document).ready(function(){
+// 	$("#my-search").click(function(){
+// 		$("#search").show(function(){
+// 			$("#search").css("display": "inline-block");
+// 		});
+// 	});
+// });
